6857f7d56fba2ef5c0cff39086ff2354e5e0af24
Jessica Castro <jessica.castros@outlook.com>
2021-05-22 03:37:41 -0300

    Specifications module, import with multer and swagger documentation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,48 @@
-class ImportCategoryUseCase {
-  constructor() {}
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import { ICategoriesRepository } from '../../repositories/implementations/ICategoriesRepository';
 
-  execute(file: any) {
-    console.log(file)
+interface IImportCategory {
+  name: string;
+  description: string;
+};
+
+class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile.on('data', async (line) => {
+        const [ name, description ] = line;
+
+        categories.push({ name, description });
+      })
+      .on('end', () => resolve(categories))
+      .on('error', (error) => reject(error));
+
+      return categories;
+      });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+
+    categories.map(category => {
+      const { name } = category;
+
+      const categoryExists = this.categoriesRepository.findByName(name);
+
+      if (!categoryExists) {
+        this.categoriesRepository.create(category);
+      }
+    })
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 
+import swaggerUI from 'swagger-ui-express';
+
+import swaggetFile from './swagger.json';
+
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggetFile));
 
 app.get('/', (request, response) => response.json({ message: 'API works! Go code!' }));
 
