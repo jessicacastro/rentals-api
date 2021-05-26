bf97e46a286d5badaaf45467c87ea7646230b250
Jessica Castro <jessica.castros@outlook.com>
2021-05-26 00:14:52 -0300

    [Feature] Authentication: Inserting authetication with jwt on node

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import 'reflect-metadata';
+
+import { container } from "tsyringe";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { password, username } = request.body;
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase)
+
+    const authInfo = await authenticateUserUseCase.execute({ username, password });
+
+    return response.status(201).json(authInfo);
+  }
+}
+
+export { AuthenticateUserController }
---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  username: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+    username: string;
+  },
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ username, password }: IRequest): Promise<IResponse> {
+    // Verificar se o usuário existente
+    const user = await this.usersRepository.findByUsername(username);
+
+    if (!user) {
+      throw new Error("Username or password incorrect!");
+    }
+
+    // Verificar se a senha está correta
+    const passwordMatch = compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Username or password incorrect!");
+    }
+
+    //Gerar o JSONWEBTOKEN
+    const token = sign({}, "7768a56f3f6fb3a6f6cc6d96c4f39ddd", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+        username: user.username
+      }
+    }
+
+    return tokenReturn;
+  }
+
+};
+
+export { AuthenticateUserUseCase };
