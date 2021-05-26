b0a6fad5fdbdcad53cb119b6cdf6c37875276d56
Jessica Castro <jessica.castros@outlook.com>
2021-05-26 12:39:40 -0300

    [Feature] Middlewares: Inserting middleware to ensure authentication

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationsController } from '../modules/cars/useCases/createSpecification/CreateSpecificationsController';
 import { ListSpecificationsController } from '../modules/cars/useCases/listSpecifications/ListSpecificationsController';
 
 const specificationsRoutes = Router();
 
+
 const createSpecificationsController = new CreateSpecificationsController();
 const listSpecificationsController = new ListSpecificationsController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationsController.handle);
 specificationsRoutes.get("/", listSpecificationsController.handle);
 
