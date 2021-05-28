c1c675a08f2bcbe4bb55b71f422352d071eb4342
Jessica Castro <jessica.castros@outlook.com>
2021-05-28 16:02:47 -0300

    [Test] Categories test in memory

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,41 @@
+
+import { AppError } from "../../../../error/AppError";
+import { Category } from "../../entities/Category";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("CreateCategoryUseCase", () => {
+
+  beforeAll(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category test description"
+    }
+
+    await createCategoryUseCase.execute(category);
+
+    const result = await categoriesRepositoryInMemory.findByName(category.name);
+
+    expect(result).toHaveProperty("id");
+  });
+
+  it("should not be able to create a category that already exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category test description"
+      }
+
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBe(AppError);
+  });
+});
